8b5e9786326e28e19df3e756988102f99c8691d5
es1210 <dmstn27034@gmail.com>
2024-08-14 16:44:32 +0900

    로그인 로그아웃 가드 정리, NeedLogoutInterceptor 등록

--- src/main/java/com/example/demo/vo/Rq.java
+++ src/main/java/com/example/demo/vo/Rq.java
@@ -1,27 +1,24 @@
 package com.example.demo.vo;
-
 import java.io.IOException;
-
 import com.example.demo.util.Ut;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
-
 public class Rq {
-
 	@Getter
 	private boolean isLogined;
 	@Getter
 	private int loginedMemberId;
-
 	private HttpServletRequest req;
 	private HttpServletResponse resp;
+
+	private HttpSession session;
 
 	public Rq(HttpServletRequest req, HttpServletResponse resp) {
 		this.req = req;
 		this.resp = resp;
+		this.session = req.getSession();
 
 		HttpSession httpSession = req.getSession();
 
@@ -30,7 +27,6 @@ public class Rq {
 			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
 		}
 	}
-
 	public void printHistoryBack(String msg) throws IOException {
 		resp.setContentType("text/html; charset=UTF-8");
 		println("<script>");
@@ -40,16 +36,22 @@ public class Rq {
 		println("history.back();");
 		println("</script>");
 	}
-
 	private void println(String str) {
 		print(str + "\n");
 	}
-
 	private void print(String str) {
 		try {
 			resp.getWriter().append(str);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void logout() {
+		session.removeAttribute("loginedMemberId");
+	}
+
+	public void login(Member member) {
+		session.setAttribute("loginedMemberId", member.getId());
 	}
 }
