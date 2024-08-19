b583c887cc8317f1cbc24f148fba52ba464cdc0d
es1210 <dmstn27034@gmail.com>
2024-08-19 09:47:41 +0900

    Rq 객체 관리를 spring 하도록

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -12,10 +12,12 @@ import com.example.demo.vo.ResultData;
 import com.example.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class UsrMemberController {
+	
+	@Autowired
+	private Rq rq;
 
 	@Autowired
 	private MemberService memberService;
@@ -24,7 +26,6 @@ public class UsrMemberController {
 	@ResponseBody
 	public String doLogout(HttpServletRequest req) {
 
-		Rq rq = (Rq) req.getAttribute("rq");
 
 		rq.logout();
 
@@ -32,7 +33,7 @@ public class UsrMemberController {
 	}
 
 	@RequestMapping("/usr/member/login")
-	public String showLogin() {
+	public String showLogin(HttpServletRequest req) {
 		return "/usr/member/login";
 	}
 
