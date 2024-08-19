2f320396cfccff10c217b1a639e5b3ba79e01468
es1210 <dmstn27034@gmail.com>
2024-08-19 13:48:42 +0900

    게시글 작성 폼 구현 및 처리, write 후 detail로 이동

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,27 +1,39 @@
 package com.example.demo.controller;
+
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.example.demo.service.ArticleService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Article;
 import com.example.demo.vo.ResultData;
 import com.example.demo.vo.Rq;
+
 import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class UsrArticleController {
+
 	@Autowired
 	private Rq rq;
+
 	@Autowired
 	private ArticleService articleService;
+
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(HttpServletRequest req, Model model, int id) {
+
 		Rq rq = (Rq) req.getAttribute("rq");
+
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
+
 		model.addAttribute("article", article);
+
 		return "usr/article/detail";
 	}
 
@@ -72,39 +84,65 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(HttpServletRequest req, int id) {
+
 		Rq rq = (Rq) req.getAttribute("rq");
+
 		Article article = articleService.getArticleById(id);
+
 		if (article == null) {
 			return Ut.jsHistoryBack("F-1", Ut.f("%d번 게시글은 없습니다", id));
 		}
+
 		ResultData userCanDeleteRd = articleService.userCanDelete(rq.getLoginedMemberId(), article);
+
 		if (userCanDeleteRd.isFail()) {
 			return Ut.jsHistoryBack(userCanDeleteRd.getResultCode(), userCanDeleteRd.getMsg());
 		}
+
 		if (userCanDeleteRd.isSuccess()) {
 			articleService.deleteArticle(id);
 		}
+
 		return Ut.jsReplace(userCanDeleteRd.getResultCode(), userCanDeleteRd.getMsg(), "../article/list");
 	}
+
+	@RequestMapping("/usr/article/write")
+	public String showWrite(HttpServletRequest req) {
+
+		return "usr/article/write";
+	}
+
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public ResultData<Article> doWrite(HttpServletRequest req, String title, String body) {
+	public String doWrite(HttpServletRequest req, String title, String body) {
+
 		Rq rq = (Rq) req.getAttribute("rq");
+
 		if (Ut.isEmptyOrNull(title)) {
-			return ResultData.from("F-1", "제목을 입력해주세요");
+			return Ut.jsHistoryBack("F-1", "제목을 입력해주세요");
 		}
 		if (Ut.isEmptyOrNull(body)) {
-			return ResultData.from("F-2", "내용을 입력해주세요");
+			return Ut.jsHistoryBack("F-2", "내용을 입력해주세요");
 		}
+
 		ResultData writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), title, body);
+
 		int id = (int) writeArticleRd.getData1();
+
 		Article article = articleService.getArticleById(id);
-		return ResultData.newData(writeArticleRd, "생성된 게시글", article);
+
+		return Ut.jsReplace(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), "../article/detail?id=" + id);
+
 	}
+
 	@RequestMapping("/usr/article/list")
 	public String showList(Model model) {
+
 		List<Article> articles = articleService.getArticles();
+
 		model.addAttribute("articles", articles);
+
 		return "usr/article/list";
 	}
+
 }
